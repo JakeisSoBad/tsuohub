c137ea1b09d2843881c622a848f85e7eaeb97f83
JakeisSoBad <117012709+JakeisSoBad@users.noreply.github.com>
2024-08-27 12:12:30 +0700

    Create English.lua

---
+++ English.lua
@@ -0,0 +1,500 @@
+return {
+    Title = "Tsuo Hub" ,
+    SubTitle = "Version 2.8",
+    Tab = {
+        Main = {
+            Title = "Main",
+            DiscordInvite = {
+                Name = "Join Our Discord",
+            },
+            SelectWeapon = {
+                Name = "Select Weapon",
+                Description = "Choose Your Weapon",
+            },
+            SelectFastAttackSpeed ={
+                Name = "Select Fast Attack Speed",
+                Description = "Select Your Fast Attack Speed",
+            },
+            SelectMethodFarm = {
+                Name = "Select Method Farm",
+                Description = "Choose Your Method Farm",
+            },
+            MainFarmSection = "Main Farm",
+            AutoFarmLevel = {
+                Name = "Auto Farm Level",
+                Description = "Auto Farm Level",
+            },
+            AutoFarmLevelFast = {
+                Name = "Auto Farm Level Fast 1 -> 300",
+                Description = "Auto Farm Level Fast",
+            },
+            AutoFarmNearest = {
+                Name = "Auto Farm Nearest",
+                Description = "Auto Farm Nearest",
+            },
+            BoneFarmSection = "Bone Farm",
+            BoneParagraph = "Bone :",
+            AutoFarmBone = {
+                Name = "Auto Farm Bone",
+                Description = "Auto Farm Bone",
+            },
+            AcceptQuest = {
+                Name = "Accept Quest",
+                Description = "Accept Quest",
+            },
+            AutoRandomBone = {
+                Name = "Auto Random Bone",
+                Description = "Auto Random Bone",   
+            },
+            CakeFarmSection = "Cake Farm",
+            NeedKillCakeFarmParagraph = "Need Kill :",
+            AutoFarmCakePrince = {
+                Name = "Auto Farm Cake Prince",
+                Description = "Auto Farm Cake Prince",
+            },
+            AutoSpawnCakePrince = {
+                Name = "Auto Spawn Cake Prince",
+                Description = "Auto Spawn Cake Prince",
+            },
+            ChestFarmSection = "Chest Farm",
+            AutoFarmChest ={
+                Name = "Auto Farm Chest",
+                Description = "Auto Farm Chest",
+            },
+            AutoFarmChestFast = {
+                Name = "Auto Farm Chest Fast",
+                Description = "Auto Farm Chest Fast",
+            },
+            MaterialFarmSection = "Select Material",
+            SelectMaterial = {
+                Name = "Select Material",
+                Description = "Select Material",
+            },
+            AutoFarmSelectedMaterial = {
+                Name = "Auto Farm Selected Material",
+                Description = "Auto Farm Selected Material",
+            },
+            BossFarmSection = "Boss Farm",
+            SelectBoss = {
+                Name = "Select Boss",
+                Description = "Select Boss",
+            },
+            AutoFarmSelectedBoss = {
+                Name = "Auto Farm Selected Boss",
+                Description = "Auto Farm Selected Boss",
+            },
+            MasteryFarmSection = "Mastery Farm",
+            AutoFarmFruitMastery = {
+                Name = "Auto Farm Fruit Mastery (Bug)",
+                Description = "Auto Farm Fruit Mastery (Bug)",
+            },
+            AutoFarmGunMastery = {
+                Name = "Auto Farm Gun Mastery (Bug)",
+                Description = "Auto Farm Gun Mastery (Bug)",
+            },
+            UseSkillZ = {
+                Name = "Use Skill Z",
+                Description = "Use Skill Z",
+            },
+            UseSkillX = {
+                Name = "Use Skill X",
+                Description = "Use Skill X",
+            },
+            UseSkillC = {
+                Name = "Use Skill C",
+                Description = "Use Skill C",
+            },
+            UseSkillV = {
+                Name = "Use Skill V",
+                Description = "Use Skill V",
+            },
+            UseSkillF = {
+                Name = "Use Skill F",
+                Description = "Use Skill F",
+            },
+            KillMobAtHealth = {
+                Name = "Kill Mob At (%) Health",
+            },
+        },
+        Setting = {
+            Title = "Setting",
+            SafeMode = {
+                Name = "Safe Mode",
+                Description = "Safe Mode",
+            },
+            WhiteScreen = {
+                Name = "White Screen",
+                Description = "White Screen",
+            },
+            BlackScreen = {
+                Name = "Black Screen",
+                Description = "Black Screen",
+            },
+            DisableNotification = {
+                Name = "Disable Notification",
+                Description = "Disable Notification",
+            },
+            AutoTurnOnKenHaki = {
+                Name = "Auto Turn On Ken Haki",
+                Description = "Auto Turn On Ken Haki",
+            },
+            DisableDammage = {
+                Name = "Disable Dammage",
+                Description = "Disable Dammage",
+            },
+            AutoTurnOnBusoHaki = {
+                Name = "Auto Turn On Buso Haki",
+                Description = "Auto Turn On Buso Haki",
+            },
+            FastAttack = {
+                Name = "Fast Attack",
+                Description = "Fast Attack",
+            },
+            BringMob = {
+                Name = "Bring Mob",
+                Description = "Bring Mob",
+            },
+        },
+        OtherFarm = {
+            Title = "Other Farm",
+            AutoEvoRaceV2 = {
+                Name = "Auto Evo Race V2",
+                Description = "Auto Evo Race V2",
+            },
+            AutoBartiloQuest = {
+                Name = "Auto Bartilo Quest",
+                Description = "Auto Bartilo Quest",
+            },
+            AutoKillDarkBeard = {
+                Name = "Auto Kill DarkBeard",
+                Description = "Auto Kill DarkBeard",
+            },
+            AutoFarmEctoplasm = {
+                Name = "Auto Farm Ectoplasm",
+                Description = "Auto Farm Ectoplasm",
+            },
+            AutoMusketeerHat = {
+                Name = "Auto Musketeer Hat",
+                Description = "Auto Musketeer Hat",
+            },
+            AutoSerpentBow = {
+                Name = "Auto Serpent Bow",
+                Description = "Auto Serpent Bow",
+            },
+            AutoFactory = {
+                Name = "Auto Factory",
+                Description = "Auto Factory",
+            },
+            AutoSwanGlasses = {
+                Name = "Auto Swan Glasses",
+                Description = "Auto Swan Glasses",
+            },
+            AutoRainbowHaki = {
+                Name = "Auto Rainbow Haki",
+                Description = "Auto Rainbow Haki",
+            },
+            AutoHolyTorch = {
+                Name = "Auto Holy Torch",
+                Description = "Auto Holy Torch",
+            },
+            AutoCastleRaid = {
+                Name = "Auto Castle Raid",
+                Description = "Auto Castle Raid",
+            },
+            AutoFarmObservationHaki = {
+                Name = "Auto Farm Observation Haki",
+                Description = "Auto Farm Observation Haki",
+            },
+            AutoObservationHakiV2 = {
+                Name = "Auto Observation Haki V2",
+                Description = "Auto Observation Haki V2",
+            },
+            EliteHunterSection = "Elite Hunter",
+            StatusEliteHunter = {
+                Have = "Status : 🟢",
+                NotHave = "Status : 🔴",    
+            },
+            Killed = "Killed : ",
+            AutoEliteHunter = {
+                Name = "Auto Elite Hunter",
+                Description = "Auto Elite Hunter",
+            },
+            AutoSecondSea = {
+                Name = "Auto Second Sea",
+                Description = "Auto Second Sea",
+            },
+            AutoThirdSea = {
+                Name = "Auto Third Sea",
+                Description = "Auto Third Sea",
+            },
+            AutoSwordsSection = "Auto Swords",
+            AutoSaber = {
+                Name = "Auto Saber",
+                Description = "Auto Saber",
+            },
+            AutoPole = {
+                Name = "Auto Pole",
+                Description = "Auto Pole",
+            },
+            AutoTrueTripleKatana = {
+                Name = "Auto True Triple Katana",
+                Description = "Auto True Triple Katana",
+            },
+            AutoRengokuSwords = {
+                Name = "Auto Rengoku Swords",
+                Description = "Auto Rengoku Swords",
+            },
+            AutoHallowSycthe = {
+                Name = "Auto Hallow Sycthe",
+                Description = "Auto Hallow Sycthe",
+            },
+            AutoBuddySword = {
+                Name = "Auto Buddy Sword",
+                Description = "Auto Buddy Sword",
+            },
+            AutoCavander = {
+                Name = "Auto Cavander",
+                Description = "Auto Cavander",
+            },
+            AutoYama = {
+                Name = "Auto Yama",
+                Description = "Auto Yama",
+            },
+            AutoTushita = {
+                Name = "Auto Tushita",
+                Description = "Auto Tushita",
+            },
+            AutoDarkDagger = {
+                Name = "Auto Dark Dagger",
+                Description = "Auto Dark Dagger",
+            },
+            AutoCursedDualKatana = {
+                Name = "Auto Cursed Dual Katana",
+                Description = "Auto Cursed Dual Katana",
+            },
+            AutoTushitaQuest1 = {
+                Name = "Auto Tushita Quest 1",
+                Description = "Auto Tushita Quest 1",
+            },
+            AutoTushitaQuest2 = {
+                Name = "Auto Tushita Quest 2",
+                Description = "Auto Tushita Quest 2",
+            },
+            AutoYamaQuest1 = {
+                Name = "Auto Yama Quest 1",
+                Description = "Auto Yama Quest 1",
+            },
+            AutoYamaQuest2 = {
+                Name = "Auto Yama Quest 2",
+                Description = "Auto Yama Quest 2",
+            },
+            MobAuraForCDKQuest = {
+                Name = "Mob Aura For CDK Quest",
+                Description = "Mob Aura For CDK Quest",
+            },
+            AutoMeleeSection = "Get Melee",
+            AutoGodhuman = {
+                Name = "Auto Godhuman",
+                Description = "Auto Godhuman",
+            },
+            AutoSuperhuman = {
+                Name = "Auto Superhuman",
+                Description = "Auto Superhuman",
+            },
+            AutoElectricClaw = {
+                Name = "Auto Electric Claw",
+                Description = "Auto Electric Claw",
+            },
+            AutoDeathStep = {
+                Name = "Auto Death Step",
+                Description = "Auto Death Step",
+            },
+            AutoSharkmanKarate = {
+                Name = "Auto Sharkman Karate",
+                Description = "Auto Sharkman Karate",
+            },
+            AutoDragonTalon = {
+                Name = "Auto Dragon Talon",
+                Description = "Auto Dragon Talon",
+            },
+
+        },
+        Stats = {
+            Title = "Stats",
+            SelectStats = {
+                Name = "Select Stats",
+                Description = "Select Stats",
+            },
+            AutoStatsSelected = { 
+                Name = "Auto Stats Selected",
+                Description = "Auto Stats Selected",
+            },
+
+        },
+        Teleport = {
+            Title = "Teleport",
+            SeaTeleportSection = "Sea Teleport",
+            TeleportToSea1 = "Teleport To Sea 1",
+            TeleportToSea2 = "Teleport To Sea 2",
+            TeleportToSea3 = "Teleport To Sea 3",
+            IslandTeleportSection = "Island Teleport",
+            SelectIsland = {
+                Name = "Select Island",
+                Description = "Select Island",
+            },
+            TeleportToSelectedIsland = {
+                Name = "Teleport To Selected Island",
+                Description = "Teleport To Selected Island",
+            },
+            StopTween = "Stop Tween",
+            TeleportToAdvancedFruitDealer= "Teleport To Advanced Fruit Dealer",
+            TeleportToLegendarySwordDealer = "Teleport To Legendary Sword Dealer",
+            TeleportToMasterofEnhancement = "Teleport To Master of Enhancement",
+            FindFullMoonHop = "Find Full Moon Hop",
+            GoToMirageIsland = "Go To Mirage Island",
+            FindMirageIslandHop= "Find Mirage Island Hop",
+        },
+        Player = {
+            Title = "Player",
+            ChoosePlayer = {
+                Name = "Choose Player",
+                Description = "Choose Player",
+            },
+            RefreshPlayersDropdown = "Refresh Players Dropdown",
+            TeleportToPlayer = {
+                Name = "Teleport To Player",
+                Description = "Teleport To Player",
+            },
+            WalkOnWater = {
+                Name = "Walk On Water",
+                Description = "Walk On Water",
+            },
+            SeaTeleport = {
+                Name = "Sea Teleport",
+                Description = "Sea Teleport",
+            },
+
+        },
+        Fruit = {
+            Title = "Fruit",
+            SelectFruit = {
+                Name = "Select Fruit",
+                Description = "Select Fruit",
+            },
+            BuyFruitSniper = {
+                Name = "Buy Fruit Sniper",
+                Description = "Buy Fruit Sniper",
+            },
+            StoreFruit = {
+                Name = "Store Fruit", 
+                Description = "Store Fruit",                
+            },
+            RandomFruit = {
+                Name = "Random Fruit", 
+                Description = "Random Fruit",
+            },
+            CollectDevilFruit = {
+                Name = "Collect Devil Fruit", 
+                Description = "Collect Devil Fruit",
+            },
+        },
+        Raid = {
+            Title = "Raid",
+            ChooseChip = {
+                Name = "Choose Chip", 
+                Description = "Choose Chip",
+            },
+            BuyChip = {
+                Name = "Buy Chip", 
+                Description = "Buy Chip",
+            },
+            AutoStartRaid = {
+                Name = "Auto & Start Raid", 
+                Description = "Auto & Start Raid",
+            },
+            KillAura = {
+                Name = "Kill Aura", 
+                Description = "Kill Aura",
+            },
+            AutoNextIsland = {
+                Name = "Next Island", 
+                Description = "Next Island",
+            },
+            AutoAwakening = {
+                Name = "Auto Awakening",
+                Description = "Auto Awakening",
+            },
+            AutoUseLowBeliFruit = {
+                Name = "Auto Use Low Beli Fruit", 
+                Description = "Auto Use Low Beli Fruit",
+            },
+            AutoBuyLawRaidChip = {
+                Name = "Auto Buy Law Raid Chip", 
+                Description = "Auto Buy Law Raid Chip",
+            },
+            AutoStartLawRaid = {
+                Name = "Auto Start Law Raid", 
+                Description = "Auto Start Law Raid",
+            },
+            AutoKillLaw = {
+                Name = "Auto Kill Law", 
+                Description = "Auto Kill Law",
+            },
+        },
+        Shop = {
+            Title = "Shop",
+            AutoBuyAbility = {
+                Name = "Auto Buy Ability",
+                Description = "Auto Buy Ability",
+            },
+            BuyGear = "Buy Gear",
+            BuyGeppo = "Buy Geppo" , 
+            BuyBusoHaki = "Buy Buso Haki",
+            BuySoru = "Buy Soru",
+            BuyKenHaki = "Buy Ken Haki",
+            SelectWeaponToBuy = {
+                Name = "Select Weapon To Buy",
+                Description = "Select Weapon To Buy",
+            },
+            RefundStats = "Refund Stats",
+            RerollRace = "Reroll Race",
+        },
+        Misc = {
+            Title = "Misc",
+            HopServer = "Hop Server",
+            JoinPiratesTeam = "Join Pirates Team",
+            JoinMarinesTeam = "Join Marines Team",
+            OpenDevilShopMenu = "Open Devil Shop Menu",
+            OpenColorHakiMenu = "Open Color Haki Menu",
+            OpenTitleNameMenu = "Open Title Name Menu",
+            OpenAwakeningMenu = "Open Awakening Menu",
+            RedeemAllCodes = "Redeem All Codes",
+            ShowItems = "Show Items",
+            RemoveFog = "Remove Fog",
+            AlwaysDay = "Always Day",
+            AutoRejoin = "Auto Rejoin",
+        },
+        RaceV4 = {
+            Title = "Race",
+            AutoEnableV3 = "Auto Enable V3",
+            AutoEnableV4 = "Auto Enable V4",
+            TweenToGear = "Tween To Gear",
+            LockMoonandUseRaceSkill = "Lock Moon and Use Race Skill",
+            AutoW = "Auto W",
+            TempleOfTime = "Tween Temple Of Time",
+            TeleportAcientOne = "Teleport Acient One",
+            TeleportRaceDoor = "Teleport Race Door",
+            AutoHumanGhoulTrial = "Auto Human/Ghoul Trial",
+            AutoBuyGear = "Auto Buy Gear",
+            AutoTrial = "Auto Trials",
+            AutoQuestTrain ="Auto Quest Train",
+            AutoKillPlayerTrial = "Auto Kill Player Trial",
+        },
+        Event = {
+            Title = "Event",
+            EspKitsuneIsland = "Esp Kitsune Island",
+            GoToKitsuneIsland ="Go To Kitsune Island",
+            CollectAzureAmbers = "Collect Azure Ambers",
+        },
+    },
+}
